f5c20c7c57ed3ef771430cbd9aede1ec316a5a23
ujjwal kumar <ujjwalkumar30122000@gmail.com>
2020-05-02 17:49:17 +0530

    1.Added implementation:-design. 2.Added two child fragment inside chat fragment.

---
+++ app/src/main/java/com/codinghelper/mscii/TabsAccessorAdaptor.java
@@ -0,0 +1,46 @@
+package com.codinghelper.mscii;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabsAccessorAdaptor extends FragmentPagerAdapter {
+    TabsAccessorAdaptor(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new GlobalChat();
+            case 1:
+                return new FriendChat();
+            default:
+                return null;
+
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "Global";
+            case 1:
+                return "Friends";
+            default:
+                return null;
+
+        }
+    }
+}
--- app/src/main/java/com/codinghelper/mscii/chatFragment.java
+++ app/src/main/java/com/codinghelper/mscii/chatFragment.java
@@ -4,17 +4,23 @@ package com.codinghelper.mscii;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class chatFragment extends Fragment {
-
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabsAccessorAdaptor tabsAccessorAdaptor;
 
     public chatFragment() {
         // Required empty public constructor
@@ -25,7 +31,13 @@ public class chatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View v= inflater.inflate(R.layout.fragment_chat, container, false);
+        viewPager=(ViewPager)v.findViewById(R.id.chattab);
+        tabsAccessorAdaptor=new TabsAccessorAdaptor(getChildFragmentManager());
+        viewPager.setAdapter(tabsAccessorAdaptor);
+        tabLayout=(TabLayout)v.findViewById(R.id.main_tab);
+        tabLayout.setupWithViewPager(viewPager);
+        return v;
     }
 
 }
