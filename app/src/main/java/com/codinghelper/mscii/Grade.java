18e1818815de32654c088e671f7da9a7e72193f7
ujjwal kumar <ujjwalkumar30122000@gmail.com>
2020-05-30 22:37:20 +0530

    added fragments in home fragment

---
+++ app/src/main/java/com/codinghelper/mscii/Grade.java
@@ -0,0 +1,35 @@
+package com.codinghelper.mscii;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Grade extends Fragment{
+
+    public Grade() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v=inflater.inflate(R.layout.fragment_grade, container, false);
+        return v;
+    }
+
+}
--- app/src/main/java/com/codinghelper/mscii/HomeFragment.java
+++ app/src/main/java/com/codinghelper/mscii/HomeFragment.java
@@ -3,18 +3,29 @@ package com.codinghelper.mscii;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,45 +36,55 @@ public class HomeFragment extends Fragment {
     public HomeFragment() {
         // Required empty public constructor
     }
-
+    private boolean loadFragment(Fragment fragment){
+        if(fragment!=null){
+            getChildFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.nav_host_fragment_home,fragment)
+                    .commit();
+        }
+        return false;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
-
         View v = inflater.inflate(R.layout.fragment_home, container, false);
-      //  androidx.appcompat.widget.Toolbar toolbar = v.findViewById(R.id.navigation_toolbar);
-      //  ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
-      //  ActionBar actionBar=((AppCompatActivity)getActivity()).getSupportActionBar();
-      //  actionBar.setDisplayHomeAsUpEnabled(true);
-      //  actionBar.setTitle("Welcome User");
+        BottomNavigationView navView = v.findViewById(R.id.nav_view_home);
+
+       navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+           @Override
+           public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+               Fragment fragment=null;
+
+               if(menuItem.getItemId()==R.id.navigation_explore){
+                   fragment=new Explore();
+
+
+
+               }
+               else if(menuItem.getItemId()==R.id.navigation_activities){
+                   fragment=new Activities();
+                /*   FragmentManager fragmentManager=getFragmentManager();
+                   FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                   fragmentTransaction.replace(R.id.nav_host_fragment_home,fragment).addToBackStack(null).commit();*/
+
+
+               }
+               else if(menuItem.getItemId()==R.id.navigation_grade){
+                   fragment=new Grade();
+                 /*  FragmentManager fragmentManager=getFragmentManager();
+                   FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                   fragmentTransaction.replace(R.id.nav_host_fragment_home,fragment).addToBackStack(null).commit();*/
+
+
+               }
+               return loadFragment(fragment);
+           }
+       });
+        loadFragment(new Explore());
         return v;
     }
-
-//    public void onBackPressed() {
-//        if (getActivity() != null) {
-////            getActivity().finish();
-//            AlertDialog.Builder alertDialogueBuilder = new AlertDialog.Builder(getContext());
-//            alertDialogueBuilder.setTitle("Confirm Exit..!!");
-//            alertDialogueBuilder.setMessage("Are you sure want to exit");
-//            alertDialogueBuilder.setCancelable(false);
-//            alertDialogueBuilder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface dialogInterface, int i) {
-//                    getActivity().finish();
-//                    Toast.makeText(getActivity(),"Developed by team SUPRD",Toast.LENGTH_SHORT).show();
-//                }
-//            });
-//            alertDialogueBuilder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface dialogInterface, int i) {
-//                    Toast.makeText(getActivity(),"You clicked NO",Toast.LENGTH_SHORT).show();
-//                }
-//            });
-//        }
-//    }
 
 
 }
