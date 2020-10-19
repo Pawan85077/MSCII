b060461afc7b7bcb77c09b6d508647ac86d5708b
Sandeep kumar <sandeepkarmkar49@gmail.com>
2020-10-19 12:56:40 +0530

    Student dashboard layout update 1.003

---
+++ app/src/main/java/com/codinghelper/mscii/UserDocumentView.java
@@ -0,0 +1,14 @@
+package com.codinghelper.mscii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserDocumentView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_document_view);
+    }
+}
--- app/src/main/java/com/codinghelper/mscii/dashboardFragment.java
+++ app/src/main/java/com/codinghelper/mscii/dashboardFragment.java
@@ -1,23 +1,69 @@
 package com.codinghelper.mscii;
 
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
-
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class dashboardFragment extends Fragment {
+
+    private Uri filePathx,resultUrix;
+    StorageReference storageUtility,storageTenth,storageTwelfth,storageGraduation;
+    FirebaseAuth firebaseAuthx;
+    FirebaseUser currentUser;
+    DatabaseReference reference;
+
+
+    //Upload Buttons
+    private Button resume;
+    private Button thumbImpression;
+
+    //Documents cards Views
+    private CardView resumeCard;
+
+    //pick variable
+    private static final int pick = 2;
+
+    //Doc_Category and doc_Type
+    static String docCategory = "";
+    static String docType = "";
+
+    static String transferrableImageURL;
 
 
     public dashboardFragment() {
@@ -31,6 +77,21 @@ public class dashboardFragment extends Fragment {
         // Inflate the layout for this fragment
         super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference= FirebaseDatabase.getInstance().getReference().child("studentDetail").child(currentUser.getUid());
+        storageUtility = FirebaseStorage.getInstance().getReference().child("utility_Documents/");
+        storageTenth = FirebaseStorage.getInstance().getReference().child("tenth_Documents/");
+        storageTwelfth = FirebaseStorage.getInstance().getReference().child("twelfth_Documents/");
+        storageGraduation = FirebaseStorage.getInstance().getReference().child("graduation_Documents/");
+
+
+        //Defining upload buttons
+        resume = (Button) rootView.findViewById(R.id.resume);
+        thumbImpression = (Button) rootView.findViewById(R.id.thumbImpression);
+
+        //Defining cardViews
+        resumeCard = (CardView)rootView.findViewById(R.id.resumeCard);
 
         //Handling Expand and Collapse
         RelativeLayout UtilityDocHeader = (RelativeLayout) rootView.findViewById(R.id.utilityDocHeader);
@@ -97,7 +158,35 @@ public class dashboardFragment extends Fragment {
             }
         });
 
+        //Card Views click action
+        resumeCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                docCategory = "utilityDocuments";
+                docType = "resume";
+                final String s = "Resume";
+                getImageAndOpenIt(docCategory,docType,s);
+            }
+        });
 
+        //Upload Buttons Action
+        resume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                docCategory = "utilityDocuments";
+                docType = "resume";
+                openGallery();
+            }
+        });
+
+        thumbImpression.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                docCategory = "utilityDocuments";
+                docType = "thumbImpression";
+                openGallery();
+            }
+        });
 
 
 
@@ -106,8 +195,111 @@ public class dashboardFragment extends Fragment {
         return rootView;
     }
 
+    //function for opening Gallery
+    private void openGallery() {
+        Intent gallery = new Intent();
+        gallery.setType("image/*");
+        gallery.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(gallery,pick);
+    }
 
-//    private void initToolbar() {
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==pick && resultCode== Activity.RESULT_OK && data!=null){
+            Uri image = data.getData();
+            CropImage.activity(image)
+                    .start(getContext(), this);
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == Activity.RESULT_OK) {
+                Uri resultUri = result.getUri();
+
+                //uploading on cropping
+                if(resultUri!=null){
+                    final ProgressDialog progressDialog=new ProgressDialog(getContext());
+                    progressDialog.setTitle("Uploading your Document...");
+                    progressDialog.show();
+                    final StorageReference ref=storageUtility.child(currentUser.getUid() + docType);
+                    ref.putFile(resultUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getContext().getApplicationContext(),"Document Uploaded",Toast.LENGTH_SHORT).show();
+                            ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    reference.child("personalDocuments").child(docCategory).child(docType).setValue(String.valueOf(uri))
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Toast.makeText(getContext().getApplicationContext(),"Database updated Successfully...",Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+
+                                }
+                            });
+                        }
+                    })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(getContext().getApplicationContext(),"Failed!"+e.getMessage(),Toast.LENGTH_SHORT).show();
+
+                                }
+                            })
+                            .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                                    double progress=(100.0*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                                    progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                                }
+                            });
+                }
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
+
+    //snack bar when image is not uploaded
+    private void notUploadedSnackBar(String s) {
+        Snackbar snackbar = Snackbar.make(getView(), "Opps.. Seems like your\n" + s +" is not uploaded yet...!!!", Snackbar.LENGTH_LONG)
+                .setAction("UPLOAD", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        openGallery();
+                    }
+                });
+        snackbar.show();
+    }
+
+    //function to get image url and open in another layout..
+    private void getImageAndOpenIt(String docCategory,String docType,String s){
+        final String docTypex = docType;
+        final String sx = s;
+        DatabaseReference ref = reference.child("personalDocuments").child(docCategory);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                transferrableImageURL = String.valueOf(dataSnapshot.child(docTypex).getValue());
+                if (transferrableImageURL.isEmpty()){
+                    notUploadedSnackBar(sx);
+                }else
+                    Toast.makeText(getContext().getApplicationContext(), ""+transferrableImageURL, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    //    private void initToolbar() {
 //        Toolbar toolbar = (Toolbar)getView().findViewById(R.id.toolbar);
 //        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
 //        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
